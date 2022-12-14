9c8a73126fa4d506dccb5c9f2b0bfc683abb36dc
staykirak <omar.farooq023@gmail.com>
2022-12-14 12:04:00 +0530

    Insertion sort added

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] ar = { 9, 3, 9, 2, 6, 0, 8, 4, 7, 1 };
+
+        insertionSort(ar);
+
+        System.out.println(Arrays.toString(ar));
+
+        sc.close();
+    }
+
+    private static void insertionSort(int[] ar) {
+
+        for (int j = 1; j < ar.length; j++) {
+            int curr = ar[j];
+            int i = j - 1;
+            while ((i > -1) && (ar[i] > curr)) {
+                ar[i + 1] = ar[i];
+                i--;
+            }
+            ar[i + 1] = curr;
+        }
+
+    }
+
+}
